a2bcce8ad88ed3dd73aa0780573a84fe44d33e17
yinlainghui <yinlianghui@hotoa.com>
2019-07-15 15:13:34 +0800

    移除无用代码

--- src/router.js
+++ src/router.js
@@ -1,43 +1,11 @@
 import express from 'express';
-import { getHtmlContent, saveReportToHtmlFile } from './html';
 import bodyParser from 'body-parser';
 import { getReportsConfig, getReport, getJsreport } from './index';
 
 const routes = express();
 const rootUrl = "/plugins/jsreport";
-const apiUrl = `${rootUrl}/api`;
 
 routes.use(bodyParser.json());
-
-// 获取报表模板
-routes.get(`${apiUrl}/html/:report_id`, async (req, res) => {
-  let report_id = req.params.report_id;
-  let report = getReport(report_id);
-  let htmlContent = getHtmlContent(report);
-  res.send(htmlContent);
-});
-
-// 报表html模板保存
-routes.post(`${apiUrl}/html/:report_id`, async (req, res) => {
-  let report_id = req.params.report_id;
-  let report = getReport(report_id).toConfig();
-  saveReportToHtmlFile(report.html_file, req.body);
-  res.send({});
-});
-
-// 获取报表数据
-routes.get(`${apiUrl}/data/:report_id`, async (req, res) => {
-  let report_id = req.params.report_id;
-  let report = getReport(report_id);
-  let data = await report.getData();
-  res.send(data);
-});
-
-// 获取报表列表
-routes.get(`${apiUrl}/reports`, async (req, res) => {
-  let reports = getReportsConfig();
-  res.send(reports);
-});
 
 // 报表查看列表界面
 routes.get(`${rootUrl}/web`, async (req, res) => {
@@ -90,9 +58,5 @@ routes.get(`${rootUrl}/web/viewer/:report_id`, async (req, res) => {
   res.send(resp.content.toString());
   res.end();
 });
-
-// routes.use(rootUrl, requestHandler);
-
-// routes.use(rootUrl, express.static(path.resolve(__dirname, "../build")));
 
 export default routes;
