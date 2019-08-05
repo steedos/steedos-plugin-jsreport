90a5930bd2818a8e9a9219d516965fce33be027e
yinlainghui <yinlianghui@hotoa.com>
2019-08-05 16:50:31 +0800

    时区问题DEMO，增加日期类型过滤条件demo

---
+++ src/contracts-utc-date.report.helper.js
@@ -0,0 +1,34 @@
+/**
+Define global helper functions for the handlebars file that named xxx.report.html.
+See https://jsreport.net/learn/handlebars to learn more.
+
+For example you want to have an upper case helper function. You can register a global function inside a helpers field with the following code:
+
+function toUpperCase(str) {
+    return str.toUpperCase();
+}
+
+And then you can call function in handlebars using:
+
+say hello world loudly: {{{toUpperCase "hello world"}}}
+ */
+function now() {
+    return new Date().toLocaleDateString()
+}
+
+function userFiltersQuery(user_filters) {
+    if (user_filters){
+        return encodeURI(JSON.stringify(user_filters));
+    }
+    else{
+        return "";
+    }
+}
+
+function localeDateString(date) {
+    return date.toLocaleDateString()
+}
+
+function localeDateTimeString(date) {
+    return date.toLocaleString()
+}
